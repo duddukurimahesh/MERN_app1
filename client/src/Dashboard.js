057fad93b8555857d1e4d6c4ce34c5c3f744fd3b
NEEV <neev@NEEVs-MacBook-Air.local>
2021-08-30 17:31:57 +0530

    created daskboard component.

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,12 +1,54 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom';
+import React, {useState, useEffect} from 'react'
+import {Link, Redirect} from 'react-router-dom';
+import axios from 'axios';
 
 const Dashboard = () => {
 
+    const [data, setData] = useState([]);
+    useEffect(() =>{
+        axios.get('http://localhost:5001/userssList', {
+            headers: { 'authToken': localStorage.getItem('authToken') }
+        })
+        .then(res => {
+            console.log("Users List---", res);
+            setData(res.data)
+        })
+    },[])
+    if(!localStorage.getItem('authToken')){
+        return <Redirect to="/login"/>
+    }
+
     return (
         <div>
-            
-           <h1>Dashboard Component</h1>
+            <nav>
+                <h1>
+                    Welcome to the MERN stack Dashboard Page.
+                </h1>
+                
+                    <button> <Link to="/register">My Profile</Link></button>
+                    <br></br>
+                    <br></br>
+                    <button> <Link to="/login" onClick = {()=> localStorage.removeItem('authToken')}>LogOut</Link></button>
+                
+            </nav>
+            <div>
+                {data.length>=1 ? 
+                
+                    data.map(profile => <div>
+                        <h3>{profile.firstName}</h3>
+                        <h3>{profile.lastName}</h3>
+                        <h3>{profile.email}</h3>
+                        <h3>{profile.mobileNum}</h3>
+                        <h3>{profile.skills}</h3>
+                        <br></br>
+                        <hr></hr>
+
+
+                    </div>)
+
+                : <p>No profiles in our application.</p>}
+            </div>
+
         </div>
     )
 }
